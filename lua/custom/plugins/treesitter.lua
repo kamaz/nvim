be0a0b92c816993ebb13c5acfd6fa74a99931daa
Kamaz <kamaz@xsimple.org>
2024-12-23 23:14:16 +0000

    fix treesitter and update list

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,33 +1,43 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			require("lazy").setup({ {
-				"nvim-treesitter/nvim-treesitter",
-				build = ":TSUpdate",
-				config = function()
-					local configs = require("nvim-treesitter.configs")
-					configs.setup({
-						ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "typescript", "go", "gosum", "goctl", "gotmpl", "gowork", "gomod", "markdown", "python", "hcl", "json", "cpp", "bash", "json5" },
-						sync_install = false,
-						highlight = { enable = true },
-						indent = { enable = true },
-						autopairs = {
-							enable = true,
-						},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+      "windwp/nvim-ts-autotag",
+      "vrischmann/tree-sitter-templ",
+    },
+    config = function()
+      vim.treesitter.language.register("templ", "templ")
+      local configs = require("nvim-treesitter.configs")
 
-						rainbow = {
-							enable = true,
-							extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-							max_file_lines = nil, -- Do not enable for files with more than n lines, int
-						},
-						playground = {
-							enable = true
-						}
-					})
-				end
-			} })
-		end
-	}
+      configs.setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "typescript", "go", "gosum", "goctl", "gotmpl", "gowork", "gomod", "markdown", "python", "hcl", "json", "cpp", "bash", "json5" },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<M-space>",
+          },
+        },
+        autopairs = {
+          enable = true,
+        },
+
+        rainbow = {
+          enable = true,
+          extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+          max_file_lines = nil, -- Do not enable for files with more than n lines, int
+        },
+        playground = {
+          enable = true
+        }
+      })
+    end
+  }
 }
--- plugin/options.lua
+++ plugin/options.lua
@@ -1,4 +1,5 @@
 vim.opt.clipboard = "unnamedplus"
+
 vim.opt.cmdheight = 1                                  -- more space in the neovim command line for displaying messages
 vim.opt.completeopt = { "menuone", "noselect" }        -- mostly just for cmp
 vim.opt.conceallevel = 0                               -- so that `` is visible in markdown files
@@ -30,12 +31,13 @@ vim.opt.laststatus = 3                                 -- only the last window w
 vim.opt.showcmd = false                                -- hide (partial) command in the last line of the screen (for performance)
 vim.opt.ruler = true                                   -- hide the line and column number of the cursor position
 vim.opt.numberwidth = 4                                -- minimal number of columns to use for the line number {default 4}
-vim.opt.signcolumn = "yes"                                                  -- always show the sign column, otherwise it would shift the text each time
+vim.opt.signcolumn =
+"yes"                                                  -- always show the sign column, otherwise it would shift the text each time
 vim.opt.wrap = false                                   -- display lines as one long line
 vim.opt.scrolloff = 8                                  -- minimal number of screen lines to keep above and below the cursor
 vim.opt.sidescrolloff = 8                              -- minimal number of screen columns to keep to the left and right of the cursor if wrap is `false`
 -- vim.opt.guifont = "monospace:h17"                      -- the font used in graphical neovim applications
-vim.opt.guifont = "JetBrainsMono\\ NFM:h10"            -- the font used in graphical neovim applications
+vim.opt.guifont = "JetBrainsMono\\ NFM:h12"            -- the font used in graphical neovim applications
 vim.opt.fillchars.eob = " "                            -- show empty lines at the end of a buffer as ` ` {default `~`}
 vim.opt.shortmess:append "c"                           -- hide all the completion messages, e.g. "-- XXX completion (YYY)", "match 1 of 2", "The only match", "Pattern not found"
 vim.opt.whichwrap:append("<,>,[,],h,l")                -- keys allowed to move to the previous/next line when the beginning/end of line is reached
@@ -43,3 +45,6 @@ vim.opt.iskeyword:append("-")                          -- treats words with `-` 
 vim.opt.formatoptions:remove({ "c", "r", "o" })        -- This is a sequence of letters which describes how automatic formatting is to be done
 vim.opt.linebreak = true
 vim.opt.breakindent = true
+vim.opt.list = true
+vim.opt.showbreak = "↳ "
+vim.opt.listchars = { tab = "→ ", eol = "↲", nbsp = "␣", trail = "•", extends = "⟩", precedes = "⟨", space = "·" }
